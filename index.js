e133f7a7a83a7f4f974dce72db20b278dcd98d40
AlexZockt - Gaming <alexandrosvisvikis1@gmail.com>
2025-07-01 00:51:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -117,3 +117,10 @@ client.once("ready", async () => {
 });
 
 client.login(process.env.TOKEN);
+
+require("http")
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Bot is alive!");
+  })
+  .listen(process.env.PORT || 3000);
